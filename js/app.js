a151f16ef29a4778ef6759d7b4284a69b2d6048b
Christoph Lindstädt <45950730+DataLind@users.noreply.github.com>
2020-12-30 23:31:09 +0100

    cleanup, added Circle Class

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 "use strict";
 
+import { randomIntFromInterval } from "./utils.js";
+
 //Selecting the canvas element
 const canvas = document.querySelector("canvas");
 
@@ -10,39 +12,88 @@ canvas.height = window.innerHeight;
 //Adding context to add methods for drawing
 const c = canvas.getContext("2d");
 
-const funcContainer = function () {
-  //Rectangle At Pos
-  c.fillStyle = "rgb(34, 191, 162)"; //adding style to the rectangle
-  c.fillRect(100, 100, 100, 100);
-  c.fillStyle = "rgb(80, 198, 241)";
-  c.fillRect(300, 300, 100, 150);
+class Circle {
+  type = "circle";
 
-  //Line On Path
-  c.beginPath(); //We want to start a path
-  c.moveTo(50, 300); //Where to start our line
-  c.lineTo(300, 100); //creates a line to the coordinates
-  c.lineTo(150, 600); //creates a line to the coordinates
-  c.strokeStyle = "rgb(207, 79, 79)"; //adding style to the line
-  c.stroke(); //draws the line
+  //prettier-ignore
+  constructor(x = window.innerWidth / 2, y = window.innerHeight / 2, dx = 1, dy = 1, radius = 50, color = "blue") {
+    //x and change in x
+    this.x = x;
+    this.dx = dx;
+    
+    //y and change in y
+    this.y = y;
+    this.dy = dy;
 
-  //Arc / Circle
-  c.beginPath(); //beginning a new path
-  c.arc(300, 300, 50, 0, Math.PI * 2, false); //creating the outline
-  c.stroke();
-};
-
-const randomIntFromInterval = function (min, max) {
-  // min and max included
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
-
-//draw 10 randomly positioned circles
-//prettier-ignore
-for (const i in [...Array(100)]) {
-  let x = Math.random() * window.innerWidth;
-  let y = Math.random() * window.innerHeight;
-  c.strokeStyle = `rgb(${randomIntFromInterval(0, 255)},${randomIntFromInterval(0,255)},${randomIntFromInterval(0, 255)})`;
-  c.beginPath(); //beginning a new path
-  c.arc(x, y, 50, 0, Math.PI * 2, false); //creating the outline
-  c.stroke();
+    //circle properties
+    this.radius = radius;
+    this.color = color;
+    
+    activeCircles.push(this);
 }
+
+  draw() {
+    //setting the color
+    c.strokeStyle = this.color;
+    c.fillStyle = this.color;
+
+    //draw the circle
+    c.beginPath(); //beginning a new path
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false); //creating the outline
+    c.stroke();
+    c.fill();
+  }
+
+  update() {
+    //check for x bounce
+    if (this.x + this.radius > window.innerWidth || this.x - this.radius < 0) {
+      this.dx = -this.dx;
+
+      //change color to red while bouncing on x
+      this.color = "red";
+    }
+
+    //check for y bounce
+    if (this.y + this.radius > window.innerHeight || this.y - this.radius < 0) {
+      this.dy = -this.dy;
+
+      //change color to green while bouncing on y
+      this.color = "green";
+    }
+
+    //Update position
+    this.x += this.dx;
+    this.y += this.dy;
+
+    //Draw new circle
+    this.draw();
+  }
+}
+
+//tracking all active circles
+const activeCircles = [];
+
+const nCircles = 35;
+//creating n Circles
+for (const i in [...Array(nCircles)]) {
+  //random values
+  let radius = randomIntFromInterval(5, 100); //circle radius
+
+  let x = Math.random() * (innerWidth - radius * 2) + radius; //x coordinate
+  let y = Math.random() * (innerHeight - radius * 2) + radius; //y coordinate
+
+  let dx = (Math.random() - 0.5) * 8; //change in x
+  let dy = (Math.random() - 0.5) * 8; //change in y
+  new Circle(x, y, dx, dy, radius);
+}
+
+const animateCircle = function () {
+  requestAnimationFrame(animateCircle); //animation works by "refreshing" the page a certain amount of time -> fps
+  c.clearRect(0, 0, innerWidth, innerHeight); //clearing the entire canvas
+
+  activeCircles.forEach((circle) => {
+    circle.update();
+  });
+};
+
+animateCircle();
