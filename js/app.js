ffeea6e0c99bda9e94c6fdbfc25c6b2fe136b925
Christoph Lindstädt <45950730+DataLind@users.noreply.github.com>
2020-12-30 20:55:31 +0100

    removed border, turned it into random circle gen

--- js/app.js
+++ js/app.js
@@ -10,21 +10,39 @@ canvas.height = window.innerHeight;
 //Adding context to add methods for drawing
 const c = canvas.getContext("2d");
 
-//Rectangle At Pos
-c.fillStyle = "rgb(34, 191, 162)"; //adding style to the rectangle
-c.fillRect(100, 100, 100, 100);
-c.fillStyle = "rgb(80, 198, 241)";
-c.fillRect(300, 300, 100, 150);
+const funcContainer = function () {
+  //Rectangle At Pos
+  c.fillStyle = "rgb(34, 191, 162)"; //adding style to the rectangle
+  c.fillRect(100, 100, 100, 100);
+  c.fillStyle = "rgb(80, 198, 241)";
+  c.fillRect(300, 300, 100, 150);
 
-//Line On Path
-c.beginPath(); //We want to start a path
-c.moveTo(50, 300); //Where to start our line
-c.lineTo(300, 100); //creates a line to the coordinates
-c.lineTo(150, 600); //creates a line to the coordinates
-c.strokeStyle = "rgb(207, 79, 79)"; //adding style to the line
-c.stroke(); //draws the line
+  //Line On Path
+  c.beginPath(); //We want to start a path
+  c.moveTo(50, 300); //Where to start our line
+  c.lineTo(300, 100); //creates a line to the coordinates
+  c.lineTo(150, 600); //creates a line to the coordinates
+  c.strokeStyle = "rgb(207, 79, 79)"; //adding style to the line
+  c.stroke(); //draws the line
 
-//Arc / Circle
-c.beginPath(); //beginning a new path
-c.arc(300, 300, 50, 0, Math.PI * 2, false); //creating the outline
-c.stroke();
+  //Arc / Circle
+  c.beginPath(); //beginning a new path
+  c.arc(300, 300, 50, 0, Math.PI * 2, false); //creating the outline
+  c.stroke();
+};
+
+const randomIntFromInterval = function (min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+//draw 10 randomly positioned circles
+//prettier-ignore
+for (const i in [...Array(100)]) {
+  let x = Math.random() * window.innerWidth;
+  let y = Math.random() * window.innerHeight;
+  c.strokeStyle = `rgb(${randomIntFromInterval(0, 255)},${randomIntFromInterval(0,255)},${randomIntFromInterval(0, 255)})`;
+  c.beginPath(); //beginning a new path
+  c.arc(x, y, 50, 0, Math.PI * 2, false); //creating the outline
+  c.stroke();
+}
