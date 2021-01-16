086eca5ef848b8d2debd5fe6b556228dc6e3e7d3
Christoph Lindstädt <45950730+DataLind@users.noreply.github.com>
2021-01-16 11:17:14 +0100

    GUI update

--- labs/boids/js/app.js
+++ labs/boids/js/app.js
@@ -1,8 +1,8 @@
 "use strict";
 //prettier-ignore
-import { Vector2D, drawGrid, drawMousePosition, randomFloat} from "../../utils.js";
-import Boid from "./Boid.js";
+import { Vector2D, drawMousePosition, randomFloat } from "../../utils.js";
 import { Rectangle, QuadTree } from "./Quadtree.js";
+import Boid from "./Boid.js";
 
 // ------------------------------------------------------------
 // ---- Standard Setup
@@ -81,32 +81,70 @@ let param = {
   cohesionForce: 1.3,
   separationForce: 1.6,
   showPerception: false,
-  showGrid: false,
+  showMousePos: false,
   showQuadTree: false,
   avoidWalls: true,
   artMode: false,
   color: "#ff6a00",
   nBoids: 50,
+  updateBoids: true,
+  drawBoids: true,
+  treeCapacity: 5,
 };
 
+let paramFunc = {
+  clearBoids: function () {
+    flockArr = [];
+  },
+
+  spawnBoids: function () {
+    for (const i in [...Array(Math.floor(param.nBoids))]) {
+      let boid = new Boid(
+        param.color,
+        width / 2 + randomFloat(-10, 10),
+        height / 2 + randomFloat(-10, 10)
+      );
+
+      flockArr.push(boid);
+
+      quadTree.insert(boid.pos);
+    }
+  },
+};
 // ---- GUI
 let gui = new dat.GUI();
-gui.add(param, "showPerception");
-gui.add(param, "showGrid");
-gui.add(param, "showQuadTree");
-gui.add(param, "avoidWalls");
-gui.add(param, "artMode");
-gui.add(param, "maxSpeed", 1, 15);
-gui.add(param, "percRadius", 10, 200);
-gui.add(param, "maxForce", 0, 1);
-gui.add(param, "alignForce", 0, 3);
-gui.add(param, "cohesionForce", 0, 3);
-gui.add(param, "separationForce", 0, 3);
-// gui.add(param, "nBoids", 0, 200);
+let generalParams = gui.addFolder("General Parameter");
+generalParams.add(param, "showMousePos");
+generalParams.add(param, "artMode");
 
-gui.add(param, "color").onFinishChange(function (value) {
+let boidParams = gui.addFolder("Boid Parameter");
+boidParams.add(param, "showPerception");
+boidParams.add(param, "avoidWalls");
+boidParams.add(param, "maxSpeed", 1, 15);
+boidParams.add(param, "percRadius", 10, 200);
+boidParams.add(param, "maxForce", 0, 1);
+boidParams.add(param, "alignForce", 0, 3);
+boidParams.add(param, "cohesionForce", 0, 3);
+boidParams.add(param, "separationForce", 0, 3);
+boidParams.add(param, "updateBoids");
+boidParams.add(param, "drawBoids");
+boidParams.addColor(param, "color").onFinishChange(function (value) {
   param.color = value;
 });
+boidParams.add(paramFunc, "clearBoids");
+boidParams.add(param, "nBoids", 1, 200);
+boidParams.add(paramFunc, "spawnBoids");
+
+let quadParams = gui.addFolder("Quadtree Parameter");
+quadParams.add(param, "showQuadTree");
+quadParams.add(param, "treeCapacity", 1, 30);
+// gui.add(param, "nBoids", 0, 200);
+
+for (const [_, value] of Object.entries(gui.__folders)) {
+  value.open();
+}
+
+generalParams.open();
 
 let flockArr = [];
 let quadTree;
@@ -121,10 +159,13 @@ function init() {
 
   flockArr = [];
 
-  quadTree = new QuadTree(new Rectangle(0, 0, width, height), 3);
+  quadTree = new QuadTree(
+    new Rectangle(0, 0, width, height),
+    param.treeCapacity
+  );
   console.log(quadTree);
 
-  for (const i in [...Array(param.nBoids)]) {
+  for (const i in [...Array(Math.floor(param.nBoids))]) {
     let boid = new Boid(
       param.color,
       width / 2 + randomFloat(-10, 10),
@@ -149,8 +190,7 @@ function animate() {
   }
 
   //Dev Mode
-  if (param.showGrid) {
-    drawGrid(canvas, c, squareSize);
+  if (param.showMousePos) {
     drawMousePosition(c, mouse, mouseEffectArea);
   }
 
@@ -173,10 +213,15 @@ function animate() {
       param.showPerception,
       param.maxForce,
       param.maxSpeed,
-      param.avoidWalls
+      param.avoidWalls,
+      param.updateBoids,
+      param.drawBoids
     );
   });
-  quadTree = new QuadTree(new Rectangle(0, 0, width, height), 3);
+  quadTree = new QuadTree(
+    new Rectangle(0, 0, width, height),
+    param.treeCapacity
+  );
   flockArr.forEach((boid) => {
     quadTree.insert(boid.pos);
   });
